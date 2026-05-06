417a976b08149bb67faf38a5d237f5cfa0ed2374
Ana Clara Espíndola <143643837+Ana0606@users.noreply.github.com>
2026-05-06 20:59:57 -0300

    Add files via upload

--- Bodega.java
+++ Bodega.java
@@ -2,23 +2,35 @@ import java.util.Scanner;
 
 public class Bodega {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Padoca Espíndola");
 
         Scanner teclado = new Scanner(System.in);
 
         String nome;
-        float valor;
+        float valor, total, dinheiro, troco;
 
         System.out.println("Informe seu nome");
-        nome = teclado.nextLine();
+        nome = teclado.next();
 
-        do{
+        System.out.println("Informe o valor do item");
+        valor = teclado.nextFloat();
+
+        total = 0;
+
+        while(valor != 0){
+
+            total = total + valor;
+
             System.out.println("Informe o valor do item");
             valor = teclado.nextFloat();
             
-        }while(valor != 0);
+        }
 
-
+        System.out.println("Você terá que pagar: R$" + total);
+        System.out.println("Qual o valor em dinheiro?");
+        dinheiro = teclado.nextFloat();
+        troco = dinheiro - total;
+        System.out.println("Troco para " + nome + " será de: R$" + troco);
         
     }
 }
