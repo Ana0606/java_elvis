6796d77b1b4dcaedf09ff6f08186319be8f45766
Ana Clara Espíndola <143643837+Ana0606@users.noreply.github.com>
2026-05-07 10:03:47 -0300

    Add files via upload

--- numeroSorteado.java
+++ numeroSorteado.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
+import java.util.Random;
 
-public class adivinheNum {
+public class numeroSorteado {
     public static void main(String[] args) throws Exception {
         System.out.println("Adivinhe o número");
 
         int  numero, chute;
         Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
 
-        numero = 7;
+        numero = aleatorio.nextInt(100);
 
         System.out.println("Digite um número");
         chute = teclado.nextInt();
