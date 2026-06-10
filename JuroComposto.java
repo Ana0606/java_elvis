7dc3b22d0c8773397c8401101365f67b558d4da4
Ana Clara Espíndola <143643837+Ana0606@users.noreply.github.com>
2026-06-10 15:23:55 -0300

    Add files via upload

--- JuroComposto.java
+++ JuroComposto.java
@@ -11,25 +11,27 @@ public class JuroComposto {
     private static double montante;
 
     public static void main(String[] args) throws Exception {
-        video.println("**Programa de rendimentos");
+        video.println("Programa de rendimentos");
         //entradas
         aplicacao = escolherAplicacao();
         video.println("Você escolheu: " + aplicacao);
-        setValoresRendimento();
-        //lógica aritmética
-        //getResultadoMensal();
-        //saída
+        char opcao;
+
+        do{
+            opcao = escolherAplicacao().charAt(0);
+        }while (opcao == 'g');
+
         video.println("Obrigado por escolher o banco Tofu!");
     }
 
-    public static void setValoresRendimento(){
+    public static void setValores(){
         video.println("Qual o capital inicial?");
         capital = teclado.nextDouble();
         video.println("Qual o número de meses?");
         meses = teclado.nextDouble();
-        video.println("Qual o juro?");
-        juros = teclado.nextDouble();
+        video.println("Calculando seu rendimento...");
     }
+
     /* 
     public static void getResultadoMensal(){
         int mesAmes;
@@ -42,23 +44,42 @@ public class JuroComposto {
         }
     }
     */
+
     public static String escolherAplicacao(){
         String minhaAplicacao;
         System.out.println("Qual aplicação deseja?");
-        System.out.println("\n a.CBD\n b.RDB\n c.Cripto\n d.Fundos\n e.Poupança\n f.Sair\n ");
+        System.out.println("\n a.CBD\n b.RDB\n c.Cripto\n d.Fundos\n e.Poupança\n f.Inserir valores\n g.Sair\n");
         System.out.println("Informe sua opção: ");
         switch (teclado.next().charAt(0)){
             case 'a': minhaAplicacao = "CDB"; // 1.2/100 ao mês
+            video.println("Rendimento do CDB: 1.2%");
+            setValores();
             opCDB();
                 break;
             case 'b' : minhaAplicacao = "RDB"; // 0.83/100 ao mês
+            video.println("Rendimento do RDB: 0.83%");
+            setValores();
             opRDB();
                 break; 
             case 'c': minhaAplicacao = "CRIPTO"; // aleatório entre 2 e 8% ao mês
+            video.println("Rendimento da CRIPTO: 2%");
+            setValores();
+            opCripto();
                 break;
             case 'd': minhaAplicacao = "FUNDOS"; // 0.70/100 ao mês
+            video.println("Rendimento do FUNDOS: 0.70%");
+            setValores();
+            opFundos();
                 break;
             case 'e': minhaAplicacao = "POUPANÇA"; // 0,5/100 ao mês
+            video.println("Rendimento da POUPANÇA: 0.5%");
+            setValores();
+            opPoupanca();
+                break;
+            case 'f': minhaAplicacao = "INSIRA SEUS VALORES"; 
+            video.println("Insira o valor do juros");
+            setValores();
+            opInserir();
                 break;
             default: minhaAplicacao = "SAIR"; 
             // inserir poupança e atualizar default para digitação do usuário + rodar mais de uma vez até o usuário desjar sair 
@@ -67,12 +88,46 @@ public class JuroComposto {
     }
 
     public static void opCDB(){
-        video
         juros = 1.2/100;
-
+        montante = capital * Math.pow(1 + juros, meses);
+        video.println("O rendimento final será de: ");
+        video.println("R$ %.2f \n", montante);
     }
 
     public static void opRDB(){
-
+        juros = 0.83/100;
+        montante = capital * Math.pow(1 + juros, meses);
+        video.println("O rendimento final será de: ");
+        video.println("R$ %.2f \n", montante);
     }
+
+    public static void opCripto(){
+        juros = 2.0/100;
+        montante = capital * Math.pow(1 + juros, meses);
+        video.println("O rendimento final será de: ");
+        video.println("R$ %.2f \n", montante);
+    }
+
+    public static void opFundos(){
+        juros = 0.70/100;
+        montante = capital * Math.pow(1 + juros, meses);
+        video.println("O rendimento final será de: ");
+        video.println("R$ %.2f \n", montante);
+    }
+
+    public static void opPoupanca(){
+        juros = 0.5/100;
+        montante = capital * Math.pow(1 + juros, meses);
+        video.println("O rendimento final será de: ");
+        video.println("R$ %.2f \n", montante);
+    }
+
+    public static void opInserir(){
+        juros = teclado.nextDouble();
+        juros = juros/100;
+        montante = capital * Math.pow(1 + juros, meses);
+        video.println("O rendimento final será de: ");
+        video.println("R$ %.2f \n", montante);
+    }
+
 }
